e869c64b811d9b61fcd5cade068c859b3af85e83
diosyahrizal <diosyahrizal@gmail.com>
2019-06-13 18:23:21 +0700

    change createStore to Index

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import {createStore} from 'redux';
-import reducer from './store/reducer';
-import {Provider} from 'react-redux';
 import Counter from './containers/Counter/Counter';
-
 import './App.css';
 
-const store = createStore(reducer);
 
 class App extends React.Component{
   render(){
     return (
-      <Provider store={store}>
-        <div className="App">
-          <Counter/>
-        </div>
-      </Provider>
+      <div className="App">
+        <Counter/>
+      </div>
     );
   }
 }
