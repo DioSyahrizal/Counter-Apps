e6299cc83557dca5c66f12dfd6fe3a7669abdc90
diosyahrizal <diosyahrizal@gmail.com>
2019-08-17 15:48:53 +0700

    feat(chore): add redux-devtool in project

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
+import { composeWithDevTools } from "redux-devtools-extension";
+
 import { rootSaga } from "./sagas";
 import { rootReducer } from "./reducer";
 
 export function configureStore() {
     const sagaMiddleware = createSagaMiddleware();
-    const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+    const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
     sagaMiddleware.run(rootSaga);
     return store;
 }
